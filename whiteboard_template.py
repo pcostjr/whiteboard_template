a13f72e93710fd00b91adc384439bef475b8997c
Peter Costantino <36424513+pcostjr@users.noreply.github.com>
2024-12-05 09:42:14 -0500

    Initial Commit
    Uploaded template. you should change this to whiteboard.py once you receive it.

---
+++ whiteboard_template.py
@@ -0,0 +1,21 @@
+# whiteboard.py
+# description:
+# author:
+# created:
+# last update:
+
+import tkinter as tk
+from tkinter import messagebox, filedialog, colorchooser
+
+
+
+class Whiteboard:
+    
+    # this is where your code goes
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Whiteboard(root)
+    root.mainloop()
